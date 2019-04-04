29e53b99100f02fdb59442bfca57a393c8b7e8bd
MazenAbe <mabuelen@umich.edu>
2019-04-04 16:59:01 -0400

    final update

--- CollegeRegistration/CollegeRegistration/RegistrationModel.Designer.cs
+++ CollegeRegistration/CollegeRegistration/RegistrationModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\EricC\Documents\GitHub\CIS297-Winter2019\CollegeRegistration\CollegeRegistration\RegistrationModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Documents\CIS297-Winter2019-RegistrationDatabase\CollegeRegistration\CollegeRegistration\RegistrationModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
